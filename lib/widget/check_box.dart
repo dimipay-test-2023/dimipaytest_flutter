deee58e61bece40891bd0d01956e912261c9946c
dodo07070707 <aa01056328583@gmail.com>
2023-08-19 00:14:00 +0900

    checkbox color fix

--- lib/widget/check_box.dart
+++ lib/widget/check_box.dart
@@ -11,12 +11,29 @@ class CheckBox1 extends StatefulWidget {
 class _CheckBox1State extends State<CheckBox1> {
   @override
   Widget build(BuildContext context) {
-    return Checkbox(
-        value: widget.click,
-        onChanged: (value) {
-          setState(() {
-            widget.click = !widget.click;
-          });
-        });
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 2,
+          color:
+              widget.click ? const Color(0xFF2EA4AB) : const Color(0xFFD7D9D9),
+        ),
+      ),
+      child: Theme(
+        data: Theme.of(context).copyWith(
+          unselectedWidgetColor: const Color(0xFFD9D9D9),
+        ),
+        child: Checkbox(
+          value: widget.click,
+          onChanged: (value) {
+            setState(() {
+              widget.click = !widget.click;
+            });
+          },
+          checkColor: const Color(0xFF2EA4AB),
+          activeColor: Colors.white,
+        ),
+      ),
+    );
   }
 }
